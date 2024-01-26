be184dc53765414c00564b1f957e504f294c77a7
Rokas Meškauskas <153185726+RokasMesk@users.noreply.github.com>
2024-01-26 14:35:41 +0300

    Added view of all the categories, used async pipeline for auto subscriptions

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -3,8 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
 import { HttpClientModule } from '@angular/common/http';
-import { response } from 'express';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -18,7 +18,8 @@ export class AddCategoryComponent implements OnDestroy {
   private addCategorySubscription?:Subscription;
 
   model:AddCategoryRequest;
-  constructor(private categoryService: CategoryService){
+  constructor(private categoryService: CategoryService,
+    private router: Router){
     this.model = {
       name:'',
       urlHandle:''
@@ -30,7 +31,7 @@ export class AddCategoryComponent implements OnDestroy {
     this.addCategorySubscription = this.categoryService.addCategory(this.model)
     .subscribe({
       next:(response)=>{
-        console.log("Add was successful");
+        this.router.navigateByUrl('/categoriesList');
       }
     });
   }
--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule,CommonModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+  categories$?: Observable<Category[]>;
+  constructor(private categoryService: CategoryService){
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+
+  }
 
 }
