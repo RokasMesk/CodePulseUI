d86401529ed5fc5308aa4489de8ea892f1fde036
Rokas Meškauskas <153185726+RokasMesk@users.noreply.github.com>
2024-01-29 23:42:49 +0300

    getAll blogPosts view and fetching blogposts from api added

---
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,38 @@
+import { Component, Pipe } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { FormsModule } from '@angular/forms';
+import { pipe } from 'rxjs';
+import { DatePipe } from '@angular/common';
+import { BlogPostService } from '../services/blog-post.service';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-blogpost',
+  standalone: true,
+  imports: [FormsModule, DatePipe],
+  templateUrl: './add-blogpost.component.html',
+  styleUrl: './add-blogpost.component.css'
+})
+export class AddBlogpostComponent {
+  model:AddBlogPost;
+  constructor(private blogPostService: BlogPostService, private router:Router){
+    this.model = {
+      title:'',
+      shortDescription:'',
+      urlHandle:'',
+      content:'',
+      featuredImageUrl:'',
+      author:'',
+      isVisible:true,
+      publishedDate:new Date()
+    }
+  }
+  onFormSubmit():void{
+    this.blogPostService.createBlogPost(this.model).
+    subscribe({
+      next: (response) =>{
+        this.router.navigateByUrl('/blogPostsList')
+      }
+    })
+  }
+}
