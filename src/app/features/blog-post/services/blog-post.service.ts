b85c124e8661b0b16ae5773fa1642c4a72439746
Rokas Meškauskas <153185726+RokasMesk@users.noreply.github.com>
2024-02-05 18:01:12 +0300

    Adding blog details to home page

--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -28,4 +28,7 @@ export class BlogPostService {
   deleteBlogPost(id:string):Observable<BlogPost>{
     return this.http.delete<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
   }
+  getBlogPostByUrlHandle(urlHandle:string):Observable<BlogPost>{
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${urlHandle}`)
+  }
 }
